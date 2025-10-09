1df29b8d0558304a3c703600b668310fd6527c32
thebengalfiles643-lgtm <thebengalfiles643@gmail.com>
2025-10-09 12:23:20 +0530

    Delete backend directory

--- backend/src/db.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-export async function connectToDatabase(uri: string, dbName?: string) {
-  if (mongoose.connection.readyState === 1) return
-  await mongoose.connect(uri, { dbName })
-}
-
-
